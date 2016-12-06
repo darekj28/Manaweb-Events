2aedaa7f0206afc3b47a62d64adae9fefe49fb01
Ellibereth <eli.chang.iv@gmail.com>
2016-12-06 16:49:27 -0500

    asdsad

--- event_main.py
+++ event_main.py
@@ -706,6 +706,6 @@ app.jinja_env.globals.update(getAvatarUrl=getAvatarUrl)
 
 
 if __name__ == '__main__':
+    app.debug = True
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-	app.run(debug=True)
