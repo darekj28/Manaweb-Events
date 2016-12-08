0220626fcaa88db163c2ec0182f30b8a4910b274
Kenny Wong <0kennywong@gmail.com>
2016-12-08 13:30:16 -0500

    karma

--- static/components/__tests__/avatar.spec.js
+++ static/components/__tests__/avatar.spec.js
@@ -3,10 +3,16 @@ import ReactTestUtils from 'react-addons-test-utils';
 import expect from 'expect';
 import Avatar from '../Avatar.jsx';
 
+function shallowRender(Component, props) {
+  const renderer = ReactTestUtils.createRenderer();
+  renderer.render(<Component {...props}/>);
+  return renderer.getRenderOutput();
+}
+
 describe('Avatar', () => {
-	it('should have an image to display avatar', () => {
-		var component = ReactTestUtils.renderIntoDocument(<Avatar />);
-		var img = ReactTestUtils.findRenderedDOMComponentWithTag(component, 'img');
-		expect(img).toExist();
+	it('should display source image', () => {
+		const src = "../../default.png";
+		const avatar = shallowRender(Avatar, {source : src});
+		expect(avatar.props.children.props.src).toEqual("../../default.png");
 	});
 })
