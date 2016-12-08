4c5baeaf23cb0124cd16d24db790ebb68a27819f
Darek <darek@manaweb.com>
2016-12-07 20:42:17 -0500

    for ben

--- Mobile/Feed/index.android.js
+++ Mobile/Feed/index.android.js
@@ -4,50 +4,40 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React from 'react';
+import {Component} from 'react'
+import {AppRegistry, StyleSheet, TabBarIOS, View} from 'react-native';
+import ViewContainer from './app/components/ViewContainer'
+import StartNavigator from './app/navigation/StartNavigator'
+
+
+
+
 
 class Feed extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      selectedTab : "tab1"
+    }
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+        <View style = {styles.contianer}>
+          <StartNavigator 
+          />
+        </View>
+
+      )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  contianer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    backgroundColor : "#F5FCFF"
+  }
 });
 
 AppRegistry.registerComponent('Feed', () => Feed);
