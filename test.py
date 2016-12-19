24281d10a75cb466d37e526c8907942c50e55e1b
Darek <darek@manaweb.com>
2016-12-19 10:46:39 -0500

    seen posts

--- test.py
+++ test.py
@@ -11,11 +11,16 @@ post_manager = Posts()
 user_list = user_manager.getUserList()
 
 
-post_manager.deleteTable("seen_posts_table")
-post_manager.createSeenPostsTable()
+feed_name_list = post_manager.getFeedNames()
+
+for feed_name in feed_name_list:
+	post_manager.deleteTable(feed_name + "_seen_posts")
+	post_manager.createSeenPostsTable(feed_name)
+
 for userID in user_list:
 	post_manager.addUserToLastSeenTable(userID)
 
+post_manager.addUserToLastSeenTable("briank")
 
 user_manager.closeConnection()
 post_manager.closeConnection()
