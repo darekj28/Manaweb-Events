721c38522de73854641cab9a138103ceab95fefa
Darek <darek@manaweb.com>
2016-12-30 09:38:07 -0500

    hash fix

--- users.py
+++ users.py
@@ -296,6 +296,7 @@ class Users:
 		
 	def updateInfo(self, userID, field_name, field_data):
 		table_name  = self.USER_TABLE
+		
 		if field_name == 'password':
 			field_data = argon2.using(rounds=4).hash(field_data)
 		self.udb.execute(self.udb.mogrify("UPDATE " + table_name  + " SET " + field_name + " = %s WHERE userID = '" + userID + "'", (field_data,)))
