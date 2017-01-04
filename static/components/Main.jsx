24d347f957a7297df813fbe2e21a00b90e680ac4
Kenny Wong <0kennywong@gmail.com>
2017-01-03 20:36:38 -0500

    prevent routing

--- static/components/Main.jsx
+++ static/components/Main.jsx
@@ -5,6 +5,7 @@ var Route = require('react-router').Route;
 var Link = require('react-router').Link;
 var IndexRoute = require('react-router').IndexRoute;
 var browserHistory = require('react-router').browserHistory;
+import AppStore from '../stores/AppStore.jsx';
 
 import App from './Home/App.jsx';
 import CommentApp from './Comment/CommentApp.jsx';
@@ -20,14 +21,17 @@ class Main extends React.Component {
 		    </div>);
 	}
 }
-
+const checkLogin = (nextState, replace) => {
+	if (!AppStore.getCurrentUser())
+        replace(`/`);
+}
 ReactDOM.render(
 	<Router history={browserHistory}>
 	<Route path="/" component={Main}>
 		<IndexRoute component={App}/>
-	  	<Route path="comment/:comment_id" component={CommentApp}/>
-	  	<Route path="notifications" component={NotificationsApp}/>
-	  	<Route path="settings" component={SettingsApp}/>
-	  	<Route path="recovery" component = {Recovery}/>
+	  	<Route path="comment/:comment_id" component={CommentApp} onEnter={checkLogin}/>
+	  	<Route path="notifications" component={NotificationsApp} onEnter={checkLogin}/>
+	  	<Route path="settings" component={SettingsApp} onEnter={checkLogin}/>
+	  	<Route path="recovery" component={Recovery}/>
     </Route></Router>, document.getElementById('app'));
 
