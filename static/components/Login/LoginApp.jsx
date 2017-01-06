4c8db1c782f46773318b87208dee1c753db48a04
Kenny Wong <0kennywong@gmail.com>
2017-01-05 20:29:12 -0500

    filler

--- static/components/Login/LoginApp.jsx
+++ static/components/Login/LoginApp.jsx
@@ -11,13 +11,13 @@ export default class LoginApp extends React.Component {
 				<LoginNavBar/>
 				<div className="container app-container col-xs-12">
 					<div className="col-xs-6">
+						<img id="Logo" src="static/logo.png"></img>
+					</div>
+					<div className="col-xs-6">
 						<div className="page-header"><h1>Create a profile</h1></div>
 						<RegisterForm/>
 						<FacebookConnect/>
-					</div>
-					<div className="col-xs-6">
-						
-					</div>
+					</div>	
 				</div>
 			</div>
 		);
