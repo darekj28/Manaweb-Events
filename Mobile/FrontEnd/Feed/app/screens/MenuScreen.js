41ffd22892a69c6f82489243fb8847eda8dcf7f9
chialol300 <benlee830912@gmail.com>
2017-01-08 18:32:01 -0500

    A version where there's animation when switching menus. Animation works, but lags a bit. Will get rid of animation next commit.

--- Mobile/FrontEnd/Feed/app/screens/MenuScreen.js
+++ Mobile/FrontEnd/Feed/app/screens/MenuScreen.js
@@ -8,12 +8,13 @@
 import React from 'react';
 import {Component} from 'react'
 import {AppRegistry,StyleSheet,View,TouchableOpacity,TouchableHighlight,
-          Alert, Animated, TouchableWithoutFeedback, Image} from 'react-native';
+          Alert, Animated, TouchableWithoutFeedback, Image, Easing} from 'react-native';
 import _ from 'lodash'
 import Icon from 'react-native-vector-icons/FontAwesome';
 import FeedScreen from './FeedScreen'
 
 const BOTTOM_BAR_PROPORTION = 0.1
+const ANIMATE_DURATION = 100
 
 var image_res = {
     home: require('../static/menuScreen/home.png')
@@ -23,36 +24,93 @@ class MenuScreen extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      post_message_expanded: false,
-      post_message_height: new Animated.Value(50),
+        show_panel1: true,
+        show_panel2: false,
+        show_panel3: false,
+        panel_flex1: new Animated.Value(1.0),
+        panel_flex2: new Animated.Value(0.0),
+        panel_flex3: new Animated.Value(0.0)
     }
+    this._onPanel1Pressed = this._onPanel1Pressed.bind(this)
+    this._onPanel2Pressed = this._onPanel2Pressed.bind(this)
+    this._onPanel3Pressed = this._onPanel3Pressed.bind(this)
+  }
+
+  _onPanel1Pressed () {
+      this.setState({show_panel1: true})
+      setTimeout(
+          () => {
+              this.setState({show_panel2: false});
+              this.setState({show_panel3: false})
+          }, ANIMATE_DURATION)
+      var timing = Animated.timing
+      Animated.parallel([
+          timing(this.state.panel_flex1, {toValue: 1.0, easing: Easing.linear, duration: ANIMATE_DURATION}),
+          timing(this.state.panel_flex2, {toValue: 0.01, easing: Easing.linear, duration: ANIMATE_DURATION}),
+          timing(this.state.panel_flex3, {toValue: 0.01, easing: Easing.linear, duration: ANIMATE_DURATION}),
+      ]).start();
+  }
+
+  _onPanel2Pressed () {
+      this.setState({show_panel2: true})
+      setTimeout(
+          () => {
+              this.setState({show_panel1: false});
+              this.setState({show_panel3: false})
+          }, ANIMATE_DURATION)
+      var timing = Animated.timing
+      Animated.parallel([
+          timing(this.state.panel_flex1, {toValue: 0.01, easing: Easing.linear, duration: ANIMATE_DURATION}),
+          timing(this.state.panel_flex2, {toValue: 1.0, easing: Easing.linear, duration: ANIMATE_DURATION}),
+          timing(this.state.panel_flex3, {toValue: 0.01, easing: Easing.linear, duration: ANIMATE_DURATION}),
+      ]).start();
+  }
+
+  _onPanel3Pressed () {
+      this.setState({show_panel3: true})
+      setTimeout(
+          () => {
+              this.setState({show_panel1: false});
+              this.setState({show_panel2: false})
+          }, ANIMATE_DURATION)
+      var timing = Animated.timing
+      Animated.parallel([
+          timing(this.state.panel_flex1, {toValue: 0.01, easing: Easing.linear, duration: ANIMATE_DURATION}),
+          timing(this.state.panel_flex2, {toValue: 0.01, easing: Easing.linear, duration: ANIMATE_DURATION}),
+          timing(this.state.panel_flex3, {toValue: 1.0, easing: Easing.linear, duration: ANIMATE_DURATION}),
+      ]).start();
   }
 
   render() {
       return (
           <View style = {styles.container}>
-              <View style = {{backgroundColor: 'aqua', flex: 1 - BOTTOM_BAR_PROPORTION, flexDirection:'row'}}>
-                  <View style = {{backgroundColor: 'coral', flex: 1}}>
+              <View style = {{flex: 1 - BOTTOM_BAR_PROPORTION, flexDirection:'row'}}>
+                  { this.state.show_panel1 &&
+                      <Animated.View style = {{flex: this.state.panel_flex1}}>
+                          <FeedScreen></FeedScreen>
+                      </Animated.View>
+                  }
+                  { this.state.show_panel2 &&
+                  <Animated.View style = {{backgroundColor: 'black', flex: this.state.panel_flex2}}>
+                  </Animated.View>
+                    }
 
-                  </View>
-                  <View style = {{backgroundColor: 'black', flex: 1}}>
-
-                  </View>
-                  <View style = {{backgroundColor: 'red', flex: 1}}>
-
-                  </View>
+                  { this.state.show_panel3 &&
+                  <Animated.View style = {{backgroundColor: 'red', flex: this.state.panel_flex3}}>
+                  </Animated.View>
+              }
               </View>
 
               <View style = {{backgroundColor: 'bisque', flex: BOTTOM_BAR_PROPORTION, flexDirection:'row'}}>
-                  <View style = {styles.menu_image_wrapper}>
+                  <TouchableHighlight style = {styles.menu_image_wrapper} onPress={this._onPanel1Pressed}>
                       <Image  style={styles.menu_image} source={image_res.home} />
-                  </View>
-                  <View style = {styles.menu_image_wrapper}>
+                  </TouchableHighlight>
+                  <TouchableHighlight style = {styles.menu_image_wrapper} onPress={this._onPanel2Pressed}>
                       <Image  style={styles.menu_image} source={image_res.home} />
-                  </View>
-                  <View style = {styles.menu_image_wrapper}>
+                  </TouchableHighlight>
+                  <TouchableHighlight style = {styles.menu_image_wrapper} onPress={this._onPanel3Pressed}>
                       <Image  style={styles.menu_image} source={image_res.home} />
-                  </View>
+                  </TouchableHighlight>
               </View>
           </View>
       );
