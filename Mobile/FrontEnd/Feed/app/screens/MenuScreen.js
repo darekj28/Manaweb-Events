4041af26112a3894a6ce4e36dd453aea12bfb2b3
chialol300 <benlee830912@gmail.com>
2017-01-08 19:06:49 -0500

    Removed animation. Still a 1 second delay between when home button is pressed and when the screen changes.

--- Mobile/FrontEnd/Feed/app/screens/MenuScreen.js
+++ Mobile/FrontEnd/Feed/app/screens/MenuScreen.js
@@ -27,89 +27,63 @@ class MenuScreen extends Component {
         show_panel1: true,
         show_panel2: false,
         show_panel3: false,
-        panel_flex1: new Animated.Value(1.0),
-        panel_flex2: new Animated.Value(0.0),
-        panel_flex3: new Animated.Value(0.0)
     }
     this._onPanel1Pressed = this._onPanel1Pressed.bind(this)
-    this._onPanel2Pressed = this._onPanel2Pressed.bind(this)
-    this._onPanel3Pressed = this._onPanel3Pressed.bind(this)
   }
 
-  _onPanel1Pressed () {
-      this.setState({show_panel1: true})
-      setTimeout(
-          () => {
-              this.setState({show_panel2: false});
-              this.setState({show_panel3: false})
-          }, ANIMATE_DURATION)
-      var timing = Animated.timing
-      Animated.parallel([
-          timing(this.state.panel_flex1, {toValue: 1.0, easing: Easing.linear, duration: ANIMATE_DURATION}),
-          timing(this.state.panel_flex2, {toValue: 0.01, easing: Easing.linear, duration: ANIMATE_DURATION}),
-          timing(this.state.panel_flex3, {toValue: 0.01, easing: Easing.linear, duration: ANIMATE_DURATION}),
-      ]).start();
+  _onPanel1Pressed (show1, show2, show3) {
+      this.setState({show_panel1: show1})
+      this.setState({show_panel2: show2})
+      this.setState({show_panel3: show3})
   }
 
-  _onPanel2Pressed () {
-      this.setState({show_panel2: true})
-      setTimeout(
-          () => {
-              this.setState({show_panel1: false});
-              this.setState({show_panel3: false})
-          }, ANIMATE_DURATION)
-      var timing = Animated.timing
-      Animated.parallel([
-          timing(this.state.panel_flex1, {toValue: 0.01, easing: Easing.linear, duration: ANIMATE_DURATION}),
-          timing(this.state.panel_flex2, {toValue: 1.0, easing: Easing.linear, duration: ANIMATE_DURATION}),
-          timing(this.state.panel_flex3, {toValue: 0.01, easing: Easing.linear, duration: ANIMATE_DURATION}),
-      ]).start();
-  }
-
-  _onPanel3Pressed () {
-      this.setState({show_panel3: true})
-      setTimeout(
-          () => {
-              this.setState({show_panel1: false});
-              this.setState({show_panel2: false})
-          }, ANIMATE_DURATION)
-      var timing = Animated.timing
-      Animated.parallel([
-          timing(this.state.panel_flex1, {toValue: 0.01, easing: Easing.linear, duration: ANIMATE_DURATION}),
-          timing(this.state.panel_flex2, {toValue: 0.01, easing: Easing.linear, duration: ANIMATE_DURATION}),
-          timing(this.state.panel_flex3, {toValue: 1.0, easing: Easing.linear, duration: ANIMATE_DURATION}),
-      ]).start();
-  }
+  // _onPanel2Pressed () {
+  //     this.setState({show_panel1: false})
+  //     this.setState({show_panel2: true})
+  //     this.setState({show_panel3: false})
+  // }
+  //
+  // _onPanel3Pressed () {
+  //     this.setState({show_panel1: false})
+  //     this.setState({show_panel2: false})
+  //     this.setState({show_panel3: true})
+  // }
 
   render() {
       return (
           <View style = {styles.container}>
               <View style = {{flex: 1 - BOTTOM_BAR_PROPORTION, flexDirection:'row'}}>
                   { this.state.show_panel1 &&
-                      <Animated.View style = {{flex: this.state.panel_flex1}}>
+                      <View style = {{flex: 1}}>
                           <FeedScreen></FeedScreen>
-                      </Animated.View>
+                      </View>
                   }
                   { this.state.show_panel2 &&
-                  <Animated.View style = {{backgroundColor: 'black', flex: this.state.panel_flex2}}>
-                  </Animated.View>
+                  <View style = {{backgroundColor: 'black', flex: 1}}>
+                  </View>
                     }
 
                   { this.state.show_panel3 &&
-                  <Animated.View style = {{backgroundColor: 'red', flex: this.state.panel_flex3}}>
-                  </Animated.View>
+                  <View style = {{backgroundColor: 'red', flex: 1}}>
+                  </View>
               }
               </View>
 
               <View style = {{backgroundColor: 'bisque', flex: BOTTOM_BAR_PROPORTION, flexDirection:'row'}}>
-                  <TouchableHighlight style = {styles.menu_image_wrapper} onPress={this._onPanel1Pressed}>
-                      <Image  style={styles.menu_image} source={image_res.home} />
+                  <TouchableHighlight
+                      style = {styles.menu_image_wrapper}
+                      onPress={() => this._onPanel1Pressed(true, false, false)}>
+                        <Image  style={styles.menu_image} source={image_res.home} />
                   </TouchableHighlight>
-                  <TouchableHighlight style = {styles.menu_image_wrapper} onPress={this._onPanel2Pressed}>
-                      <Image  style={styles.menu_image} source={image_res.home} />
+                  <TouchableHighlight
+                      style = {styles.menu_image_wrapper}
+                      onPress={() => this._onPanel1Pressed(false, true, false)}>
+                        <Image  style={styles.menu_image} source={image_res.home} />
                   </TouchableHighlight>
-                  <TouchableHighlight style = {styles.menu_image_wrapper} onPress={this._onPanel3Pressed}>
-                      <Image  style={styles.menu_image} source={image_res.home} />
+                  <TouchableHighlight
+                      style = {styles.menu_image_wrapper}
+                      onPress={() => this._onPanel1Pressed(false, false, true)}>
+                        <Image  style={styles.menu_image} source={image_res.home} />
                   </TouchableHighlight>
               </View>
           </View>
