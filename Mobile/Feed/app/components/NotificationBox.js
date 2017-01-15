a2afe84dad5ede6812e02be6397c1ca113e90294
Kenny Wong <0kennywong@gmail.com>
2017-01-15 15:01:53 -0500

    notification

---
+++ Mobile/Feed/app/components/NotificationBox.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import {Picker, RCTAnimation, AsyncStorage, AppRegistry,StyleSheet,Text,View,ListView,
+		TouchableOpacity,TouchableHighlight, TextInput,
+          Alert, Image, Animated, TouchableWithoutFeedback, ScrollView} from 'react-native';
+import _ from 'lodash'
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default class NotificationBox extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	getNotificationSyntax(note) {
+        var whose; var also; var notification;
+        if (note.isOP) { 
+            whose = "your";
+            also = "";
+        }
+        else {
+            whose = note.op_name + "'s";
+            also = "also";
+        }
+        if (note.numOtherPeople > 1)
+            notification = note.sender_name + " and " + 
+                note.numOtherPeople + " other people commented on " + whose + " post."
+        else if (note.numOtherPeople == 1)
+            notification = note.sender_name + 
+                " and 1 other person commented on " + whose + " post."
+        else 
+            notification = note.sender_name + " " + also + " commented on " + whose + " post."
+        return notification;
+    }
+    _navigateToComment() {
+      this.props.navigator.push({
+        href: "Comment",
+        username : this.props.username,
+        comment_id : this.props.note['comment_id']
+      })
+    }
+	render() {
+		var note = this.props.note;
+		return(
+			<TouchableWithoutFeedback onPress={this._navigateToComment.bind(this)}>
+				<View style={{flex:1, justifyContent: 'flex-start', borderBottomColor: '#000000',
+	                borderBottomWidth: 1}}>
+	                <View style={{flex: 1, flexDirection:'row'}}>
+	                    <Text style = {styles.text_message}>
+	                        {this.getNotificationSyntax.bind(this)(note)} 
+	                    </Text>
+	                     <Text style = {styles.text_time}>
+	                        {note.timeString}
+	                    </Text>
+	                </View>
+	            </View>
+            </TouchableWithoutFeedback>
+			);
+	}
+}
+const styles = StyleSheet.create({
+  text_time: {
+      flex: 1,
+      fontSize: 16,
+      textAlignVertical: 'top',
+      marginLeft: 4,
+      marginTop: 6,
+      color: 'silver'
+  },
+  text_message: {
+    flex: 1,
+    fontSize: 16,
+    textAlignVertical: 'top',
+    marginLeft: 16,
+    marginBottom : 30,
+    color : '#353D41'
+  }
+});
--- Mobile/Feed/app/screens/NotificationScreen.js
+++ Mobile/Feed/app/screens/NotificationScreen.js
@@ -5,12 +5,57 @@ import {Picker, RCTAnimation, AsyncStorage, AppRegistry,StyleSheet,Text,View,Lis
 import _ from 'lodash'
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+import Notifications from '../components/Notifications'
+
 export default class NotificationScreen extends React.Component {
-	render() {
+  constructor(props) {
+      super(props);
+      this.state = {current_user : {userID : "not initialized"},
+      current_username : '',
+                  notifications : []};
+  }
+  getNotifications() {
+      let url = "https://manaweb-events.herokuapp.com"
+      let test_url = "http://0.0.0.0:5000"
+      fetch(test_url + "/mobileGetNotifications", 
+        {method: "POST",
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ comment_id : this.state.current_user })
+        }
+      ).then((response) => response.json())
+      .then((responseData) => {
+        if (responseData.notification_list.length > 0) {
+              var feed = []
+              for (var i = 0; i < responseData['notification_list'].length; i++) {
+                var obj = responseData['notification_list'][i]
+                  feed.unshift({
+                    comment_id : obj['comment_id'],
+                    timeString : obj['timeString'],
+                    isOP : obj['isOP'],
+                    numOtherPeople : obj['numOtherPeople'],
+                    sender_name : obj['sender_name'],
+                    op_name : obj['op_name']
+                })
+              }
+              this.setState({notifications: feed})
+          }    
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+  componentDidMount() {
+    this.getNotifications.bind(this)();
+  }
+  render() {
 		return (
 			<View style = {styles.container}>
-      <Text>Notifications</Text>
-            </View>
+        <Text>Your notifications</Text>
+        <Notifications notifications={this.state.notifications} username={this.state.current_username} navigator={this.props.navigator}/>
+      </View>
 			)
 	}
 }
