a59ecb56fbd22b1c573d301da486a60b81081cba
Kenny Wong <0kennywong@gmail.com>
2017-01-17 23:29:39 -0500

    recovery screen

---
+++ Mobile/Feed/app/screens/RecoveryScreen.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {Picker, RCTAnimation, AsyncStorage, AppRegistry,StyleSheet,Text,View,ListView,
+		TouchableOpacity,TouchableHighlight, TextInput,
+          Alert, Image, Animated, TouchableWithoutFeedback, ScrollView} from 'react-native';
+import _ from 'lodash'
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default class RecoveryScreen extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			email_or_phone : ""
+		};
+	}
+	handleChange(input) {
+		this.setState({ email_or_phone : input });
+	}
+	render() {
+		return (
+			<View style={styles.container}>
+				<TouchableOpacity onPress={() => this.props.navigator.pop()}>
+	                <Icon name="chevron-left" size={30} />
+	            </TouchableOpacity>
+				<View style={styles.input_box}> 
+					<TextInput onChangeText={this.handleChange.bind(this)}
+	                style={styles.input_text}
+	                placeholder="Email or phone number"/>
+	            </View>
+			</View>
+			)
+	}
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection : "column",
+    justifyContent: 'space-between',
+    padding : 10,
+    paddingTop: 40,
+    backgroundColor: "white",
+    alignItems: 'flex-start'
+  },
+  input_box: {
+    flexDirection : "row",
+    flex: 0.075,
+    borderColor: "skyblue",
+    borderWidth : 1,
+    borderRadius : 5
+  },
+  input_text :{
+    flex: 0.65,
+  }
+
+});
