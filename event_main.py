396323a3a2b42d29191ee443e26fa3792546ccef
Darek <darek@manaweb.com>
2017-01-19 17:14:03 -0500

    more processors

--- event_main.py
+++ event_main.py
@@ -326,7 +326,7 @@ def date_format(time=False):
 if __name__ == '__main__':
     app.debug = True
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, threaded = True)
+    app.run(host='0.0.0.0', port=port, processes = 128)
 
 EMPTY_STRING = ""
 
