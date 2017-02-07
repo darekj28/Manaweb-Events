d4c705178d52393413dd6683514c28f13cb04457
chialol300 <benlee830912@gmail.com>
2017-02-06 21:41:08 -0500

    Fixed facebook login page styling

--- Mobile/Feed/app/screens/register/FbCreate.js
+++ Mobile/Feed/app/screens/register/FbCreate.js
@@ -13,8 +13,10 @@ const {
 
 import React from 'react';
 import {Component} from 'react'
-import {Image, AsyncStorage, AppRegistry,StyleSheet,Text,View,ListView,TouchableOpacity,TouchableHighlight, TextInput} from 'react-native';
-
+import {Image, AsyncStorage, AppRegistry,StyleSheet,Text,View,ListView,TouchableOpacity,TouchableHighlight, TextInput, TouchableWithoutFeedback} from 'react-native';
+import {Dimensions} from 'react-native';
+import dismissKeyboard from 'react-native-dismiss-keyboard';
+import RegisterHeader from '../../components/register/RegisterHeader';
 import ViewContainer from '../../components/ViewContainer';
 import HomeStatusBar from '../../components/HomeStatusBar';
 import _ from 'lodash'
@@ -48,8 +50,8 @@ class FbCreate extends Component {
     headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json',
-        }, 
-      body: 
+        },
+      body:
       JSON.stringify(
        {
         email : this.state.email,
@@ -93,8 +95,8 @@ class FbCreate extends Component {
     headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json',
-        }, 
-      body: 
+        },
+      body:
       JSON.stringify(
        {
         username : username
@@ -136,7 +138,7 @@ class FbCreate extends Component {
       this.setState({email : result.email})
       this.setState({first_name : result.first_name})
       this.setState({last_name : result.last_name})
-    } 
+    }
   }
   clearUsername(){
     this.setState({username: ""})
@@ -160,63 +162,47 @@ class FbCreate extends Component {
 
 
   render() {
-    var error_message = this.getErrorMessage.bind(this)();
-    return (
 
-      <View style = {styles.container}>
-          <View style = {styles.top_bar}>
-              <TouchableOpacity style = {styles.back_button}
-                onPress = {() => this.props.navigator.pop()}>
-                <Icon name = "chevron-left" size = {20}/>
-              </TouchableOpacity>
-               <Image
-                style={styles.logo}
-                source={require('../../static/favicon-32x32.png')}
-              />
-              <View style = {styles.cog_box}>
-                <Icon name = "cog" size = {20} style = {styles.cog}/> 
+      var {height, width} = Dimensions.get('window');
+      var error_message = this.getErrorMessage.bind(this)();
+      return (
+          <TouchableWithoutFeedback onPress={() => dismissKeyboard()}>
+              <View style={[styles.container, {height: height}]}>
+                  <RegisterHeader navigator={this.props.navigator}/>
+                  <View style={{flex : 1, flexDirection : 'column'}}>
+                      <View style={{flex : 2}}>
+                          <View style={{flex : 1.5, alignItems : 'center', justifyContent : 'center'}}>
+                              <Text style={{fontSize : 18}}>
+                                  Welcome {this.state.first_name}!
+                              </Text>
+                          </View>
+                          <View style={{flex : 0.6}}/>
+                              <View style={{flex : 1,justifyContent : 'center'}}>
+                                  <Text style={styles.label}>Select a username</Text>
+                                  <View style={styles.input_wrapper}>
+                                      <TextInput onChangeText = {this.handleUsernameChange.bind(this)}
+                                          style = {styles.input}
+                                          value = {this.state.username}
+                                          maxLength = {15}/>
+                                  </View>
+                              </View>
+                              <View style={{flex : 0.6, width : 220, alignItems : 'center'}}>
+                                  <View style={{flex : 1, alignItems:'center', justifyContent : 'center'}}>
+                                      {error_message}
+                                  </View>
+                              </View>
+                          </View>
+                      <View style = {{flex : 0.5, alignItems : 'center'}}>
+                          <TouchableOpacity style={{flex : 1}} onPress = {this.handleUsernameSubmit.bind(this)}>
+                              <View style = {styles.button}>
+                                  <Text style={styles.button_text}>Submit!</Text>
+                              </View>
+                          </TouchableOpacity>
+                      </View>
+                      <View style = {{flex : 3}}/>
+                  </View>
               </View>
-            </View>
-            <View style = {styles.small_padding}/>
-            <View style = {styles.instruction_box}> 
-              <Text style = {styles.instruction_text}>
-                Welcome {this.state.first_name} !
-              </Text>
-            </View>
-            <View style = {styles.input_row}>
-              <View style = {styles.input_box}> 
-                <TextInput
-                onChangeText = {this.handleUsernameChange.bind(this)}
-                style = {styles.input_text} placeholder = "Select a username"
-                value = {this.state.username}
-                maxLength = {15}
-              />
-              { this.state.username != "" &&
-              <View style = {styles.clear_button}>
-                <Icon name = "close" size = {20} onPress = {this.clearUsername.bind(this)}/>
-              </View>}
-              </View>
-            </View>
-          <View style = {styles.small_padding}/>
-          { // error_message != null &&
-            false &&
-              <View style = {styles.error_box}>
-                {error_message}
-            </View>
-          }
-          {error_message == null &&
-            <View style = {styles.small_padding}/>
-          }
-            <View style = {styles.large_padding} />
-             <View style = {styles.bottom_bar}>
-                <TouchableOpacity style = {styles.next} onPress = {this.handleUsernameSubmit.bind(this)}>
-                <Text style = {styles.next_text}>
-                  Submit!
-                </Text>
-              </TouchableOpacity>
-             </View>
-          </View>
-
+          </TouchableWithoutFeedback>
     )
   }
 
@@ -224,116 +210,26 @@ class FbCreate extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection : "column",
-    justifyContent: 'space-between',
-    padding : 10,
-    paddingTop: 40,
-    backgroundColor: "white",
-  },
-
-
-  top_bar : {
-    flex : 0.05,
-    flexDirection : "row",
-    justifyContent: "space-around",
-    // backgroundColor: "coral",
-    alignItems: "center"
-  },
-
-  back_button :{
-    flex : 1,
-  },
-
-  back_button_text: {
-
-  },
-
-  logo: {
-    flex : 1,
-    resizeMode: "contain"
-  },
-
-  cog_box: {
-    flex:1,
-    flexDirection : "row",
-    justifyContent : "flex-end"
-  },
-  // cog : {
-  // },
-
-  instruction_box :{
-    flex : 0.075,
-  },
-
-  instruction_text : {
-    fontSize : 24
-  },
-
-  input_row: {
-    flexDirection: "row",
-    flex : 0.075,
-  },
-  input_box: {
-    flexDirection : "row",
-    flex: 0.075,
-    borderColor: "skyblue",
-    borderWidth : 1,
-    borderRadius : 5
-    // backgroundColor: "skyblue"
-  },
-
-  input_text :{
-    flex: 0.65,
-    padding: 5
-  },
-
-  clear_button : {
-    flex: 0.05,
-    justifyContent: "center"
-  },
-
-  large_padding : {
-    flex: 0.45,
-    backgroundColor : "white"
-  },
-
-    error_box : {
-    flex: 0.05,
-    flexDirection : "column",
-  },
-
-  error_text : {
-    color : "red",
-    fontSize : 20,
-    alignSelf: "center"
-  },
-
- 
-  bottom_bar : {
-    flex : 0.05,
-    // backgroundColor : "purple",
-    flexDirection: "row",
-    justifyContent : "flex-end",
-  },
-
-  recovery_text: {
-    flex: 0.75
-  },
- 
-  next_text : {
-    borderColor : "skyblue",
-    borderWidth : 1,
-    borderRadius : 5,
-    padding: 8,
-    textAlign : "center"
-  },
-
-  small_padding : {
-    flex : 0.05,
-  },
-
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems : 'center',
+        backgroundColor: "white",
+    },
+    label : {flex : 0, fontSize : 12, fontWeight : 'bold', color : '#696969'},
+    input_wrapper : {flex : 1, borderBottomColor : 'silver', borderBottomWidth : 1},
+    input : {flex : 1, width : 220, fontSize : 14, justifyContent : 'flex-start', paddingBottom: 0},
+    button : {
+        flex : 1,
+        backgroundColor : '#90d7ed',
+        borderRadius:60,
+        justifyContent : 'center',
+        alignItems : 'center',
+        width : 100,
+        height : 35
+    },
+    button_text : {color : 'white', fontWeight : 'bold', fontSize : 14},
+    error_text : {color : 'red', fontWeight : 'bold', fontSize : 12},
 });
 
 module.exports = FbCreate
